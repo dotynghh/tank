ecb2aa89272a04853f68811685b1d39f62a5e619
Tank <Dotyng_hh@163.com>
2016-06-28 14:49:23 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tank::Application.config.secret_token = 'f4d2e2487083c2ee7ee5f2e6dddd103f0c40c382a0c7b28d2f8818e38b4a389b67525c867e60aab8091009068eca787a90330f3b1781f4e2c8aaf26d0659fce2'
