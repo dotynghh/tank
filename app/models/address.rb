3b1ba3cc46f4896b2def2a5b837ae9a95d741afa
dotyng_hh <Dotyng_hh@163.com>
2016-07-15 09:25:25 +0800

    add validates to address,order,user

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ActiveRecord::Base
+
+  validates :consignee, presence: { message: "收获人不能为空"}
+  validates :province, presence: { message: "省份不能为空"}
+  validates :phonenumber, presence: { message: "联系电话不能为空"}
 	belongs_to :user
 end
